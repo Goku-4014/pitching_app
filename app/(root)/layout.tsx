a0a3a6fbef77609dc7e8848c324f4648cc4a5b4f
Manish Chauhan <mightymanish8@gmail.com>
2025-01-03 13:47:07 +0530

    setting up the app
    implemented next auth using github

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../componets/Navbar";
+
+
+export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
+    return (
+        <main className="font-work-sans">
+            <Navbar />
+
+            {children}
+        </main>
+    )
+}

388ecb0d827b59df1dfacfb561923ef2c6dace30
Manish Chauhan <mightymanish8@gmail.com>
2025-01-07 01:26:44 +0530

    sentry setup

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -1,133 +1,183 @@
-"use client"
-import React, { useActionState, useState } from 'react'
-import { Input } from './ui/input'
-import { Textarea } from './ui/textarea';
-import MDEditor from '@uiw/react-md-editor';
-import { Button } from './ui/button';
-import { Send } from 'lucide-react';
-import { formSchema } from '@/lib/validation';
-import { z } from 'zod';
-// import { Result } from 'postcss';
+"use client";
 
+import React, { useState, useActionState } from "react";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import MDEditor from "@uiw/react-md-editor";
+import { Button } from "@/components/ui/button";
+import { Send } from "lucide-react";
+import { formSchema } from "@/lib/validation";
+import { z } from "zod";
+import { useToast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
+import { createPitch } from "@/lib/actions";
+// import { createPitch } from "@/lib/actions";
 
 const StartupForm = () => {
-     const [errors, setErrors] = useState<Record<string,string>>({});
+  const [errors, setErrors] = useState<Record<string, string>>({});
+  const [pitch, setPitch] = useState("");
+  const { toast } = useToast();
+  const router = useRouter();
 
-     const [pitch, setPitch] = useState("**Hello world!!!**");
-     
-     const handleFormSubmit = async (prevState: any, formData: FormData) => {
-        try {
-          // Extracting values from the FormData object
-          const formValues = {
-            title: formData.get("title") as string,
-            description: formData.get("description") as string,
-            category: formData.get("category") as string,
-            link: formData.get("link") as string,
-            pitch, 
-          };
-      
-          // Validate the form values using `formSchema`
-          await formSchema.parseAsync(formValues);
-      
-          console.log("Form Values:", formValues);
-          setErrors({});
-      
-          // Example of calling a function to create an idea
-          // const result = await createIdea(prevState, formData, formValues.pitch);
-          // console.log("Result:", result);
-        } catch (error) {
-          // Log or handle validation/submission errors
-          console.error("Error:", error);
-        } finally {
-          // Cleanup logic if necessary
-        }
+  const handleFormSubmit = async (prevState: any, formData: FormData) => {
+    try {
+      const formValues = {
+        title: formData.get("title") as string,
+        description: formData.get("description") as string,
+        category: formData.get("category") as string,
+        link: formData.get("link") as string,
+        pitch,
       };
 
-      const [state, formAction, isPending] = useActionState(handleFormSubmit, {
-        error: '', // Initial error state
-        status: 'INITIAL', // Initial status state
+      await formSchema.parseAsync(formValues);
+      // console.log(formValues);
+
+      const result = await createPitch(prevState, formData, pitch);
+
+      if (result.status == "SUCCESS") {
+        toast({
+          title: "Success",
+          description: "Your startup pitch has been created successfully",
+        });
+
+        router.push(`/startup/${result._id}`);
+      }
+
+      return result;
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        const fieldErorrs = error.flatten().fieldErrors;
+
+        setErrors(fieldErorrs as unknown as Record<string, string>);
+
+        toast({
+          title: "Error",
+          description: "Please check your inputs and try again",
+          variant: "destructive",
+        });
+
+        return { ...prevState, error: "Validation failed", status: "ERROR" };
+      }
+
+      toast({
+        title: "Error",
+        description: "An unexpected error has occurred",
+        variant: "destructive",
       });
-      
-      
 
-     
+      return {
+        ...prevState,
+        error: "An unexpected error has occurred",
+        status: "ERROR",
+      };
+    }
+  };
 
+  const [state, formAction, isPending] = useActionState(handleFormSubmit, {
+    error: "",
+    status: "INITIAL",
+  });
 
   return (
-    <form action={formAction }className='startup-form'>
-        <div>
-            <label htmlFor='title' className='startup-form_label'>
-                Title
-            </label>
-            <Input id='title' name='title'  className='startup-form_input' required placeholder=' Startup title'/>
-            {errors.title && <p className='startup-form_error'>{errors.title}</p>}
+    <form action={formAction} className="startup-form">
+      <div>
+        <label htmlFor="title" className="startup-form_label">
+          Title
+        </label>
+        <Input
+          id="title"
+          name="title"
+          className="startup-form_input"
+          required
+          placeholder="Startup Title"
+        />
 
-        </div>
+        {errors.title && <p className="startup-form_error">{errors.title}</p>}
+      </div>
 
-        <div>
-            <label htmlFor='description' className='startup-form_label'>
-                Description
-            </label>
-            <Textarea id='description' name='description'  className='startup-form_textarea' required placeholder=' Startup Description'/>
-            {errors.description && <p className='startup-form_error'>{errors.description}</p>}
+      <div>
+        <label htmlFor="description" className="startup-form_label">
+          Description
+        </label>
+        <Textarea
+          id="description"
+          name="description"
+          className="startup-form_textarea"
+          required
+          placeholder="Startup Description"
+        />
 
-        </div>
+        {errors.description && (
+          <p className="startup-form_error">{errors.description}</p>
+        )}
+      </div>
 
-        <div>
-            <label htmlFor='category' className='startup-form_label'>
-                Category
-            </label>
-            <Input id='category' name='Category'  className='startup-form_input' required placeholder=' Startup Category (eg. Technology, Education...)'/>
-            {errors.category && <p className='startup-form_error'>{errors.category}</p>}
+      <div>
+        <label htmlFor="category" className="startup-form_label">
+          Category
+        </label>
+        <Input
+          id="category"
+          name="category"
+          className="startup-form_input"
+          required
+          placeholder="Startup Category (Tech, Health, Education...)"
+        />
 
-        </div>
+        {errors.category && (
+          <p className="startup-form_error">{errors.category}</p>
+        )}
+      </div>
 
-        <div>
-            <label htmlFor='link' className='startup-form_label'>
-                Image URL
-            </label>
-            <Input id='link' name='link'  className='startup-form_input' required placeholder=' Startup image URL'/>
-            {errors.link && <p className='startup-form_error'>{errors.link}</p>}
+      <div>
+        <label htmlFor="link" className="startup-form_label">
+          Image URL
+        </label>
+        <Input
+          id="link"
+          name="link"
+          className="startup-form_input"
+          required
+          placeholder="Startup Image URL"
+        />
 
-        </div>
+        {errors.link && <p className="startup-form_error">{errors.link}</p>}
+      </div>
 
+      <div data-color-mode="light">
+        <label htmlFor="pitch" className="startup-form_label">
+          Pitch
+        </label>
 
-        <div data-color-mode ='light'>
-            <label htmlFor='pitch' className='startup-form_label'>
-                Pitch
-            </label>
-            <MDEditor
-              value={pitch}
-              onChange={(value) => setPitch(value as string)}
-              id='pitch'
-              preview='edit'
-              height={300}
-              style={{borderRadius:20,overflow: "hidden"}}
-              textareaProps={{
-                placeholder: "Breifly descriibe your idea and what problems it solves"
-              }}
-              previewOptions={{
-                disallowedElements:["style"]
-              }}
+        <MDEditor
+          value={pitch}
+          onChange={(value) => setPitch(value as string)}
+          id="pitch"
+          preview="edit"
+          height={300}
+          style={{ borderRadius: 20, overflow: "hidden" }}
+          textareaProps={{
+            placeholder:
+              "Briefly describe your idea and what problem it solves",
+          }}
+          previewOptions={{
+            disallowedElements: ["style"],
+          }}
+        />
 
-            />
-           
-            {errors.pitch&& <p className='startup-form_error'>{errors.pitch}</p>}
+        {errors.pitch && <p className="startup-form_error">{errors.pitch}</p>}
+      </div>
 
-        </div>
-
-        <Button
-        type='submit'
-        className='startup-form_btn text-white'
+      <Button
+        type="submit"
+        className="startup-form_btn text-white"
         disabled={isPending}
-        >
-        {isPending ? "Submitting...": "Submit your Pitch"}
-        <Send className='size-6 ml-2'/>
-        </Button>
-
-
+      >
+        {isPending ? "Submitting..." : "Submit Your Pitch"}
+        <Send className="size-6 ml-2" />
+      </Button>
     </form>
-  )
-}
+  );
+};
 
-export default StartupForm
+export default StartupForm;
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://423d5f8c2f1a79bbf5608ac43bce120b@o4508598393503744.ingest.us.sentry.io/4508598449209344",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
